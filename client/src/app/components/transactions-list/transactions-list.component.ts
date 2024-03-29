7970d864d5d1017c28ae650f41b2cec8862f2db9
coopfeathy <cooperfeatherstone13@gmail.com>
2024-03-28 23:36:59 -0500

    update transactions list

--- client/src/app/components/transactions-list/transactions-list.component.ts
+++ client/src/app/components/transactions-list/transactions-list.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Transaction, TransactionService } from '../../services/transaction.service';
 
 @Component({
   selector: 'app-transactions-list',
   templateUrl: './transactions-list.component.html',
-  styleUrl: './transactions-list.component.css'
+  styleUrls: ['./transactions-list.component.css']
 })
-export class TransactionsListComponent {
+export class TransactionsListComponent implements OnInit {
+  transactions: Transaction[] = [];
 
+  constructor(private transactionService: TransactionService) { }
+
+  ngOnInit(): void {
+    this.transactionService.getTransactions().subscribe((transactions: Transaction[]) => {
+      this.transactions = transactions;
+    });
+  }
+
+  deleteTransaction(id: string): void {
+    this.transactionService.deleteTransaction(id).subscribe(() => {
+      this.transactions = this.transactions.filter(t => t._id !== id);
+    });
+  }
 }
