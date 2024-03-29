a20892ab4e9d6f5b7bc2e8b1329ee9005485fbde
coopfeathy <cooperfeatherstone13@gmail.com>
2024-03-28 23:48:43 -0500

    upadte transaction form

--- client/src/app/components/transaction-form/transaction-form.component.ts
+++ client/src/app/components/transaction-form/transaction-form.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { TransactionService } from '../../services/transaction.service';
 
 @Component({
   selector: 'app-transaction-form',
   templateUrl: './transaction-form.component.html',
-  styleUrl: './transaction-form.component.css'
+  styleUrls: ['./transaction-form.component.css']
 })
 export class TransactionFormComponent {
 
+  constructor(private transactionService: TransactionService) { }
+
+  onSubmit(form: NgForm): void {
+    this.transactionService.addTransaction(form.value).subscribe(result => {
+      console.log(result);
+      form.reset();
+    });
+  }
 }
